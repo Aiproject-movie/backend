d9c6151c9c3a8a94fcf16256b39c0d3b893cc2dc
Sumaya <suah0002@stud.ek.dk>
2025-11-10 10:23:43 +0100

    Initial commit

---
+++ src/test/java/com/example/aiprojectfinal/AiProjectFinalApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.aiprojectfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectFinalApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
