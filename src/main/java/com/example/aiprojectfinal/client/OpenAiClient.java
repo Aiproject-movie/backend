8ff2abc2ae8882c3fac771128de09409869b0703
Sumaya <suah0002@stud.ek.dk>
2025-11-12 19:42:25 +0100

    tilføjet dto'ere og lavet Mono, flux

--- src/main/java/com/example/aiprojectfinal/client/OpenAiClient.java
+++ src/main/java/com/example/aiprojectfinal/client/OpenAiClient.java
@@ -3,6 +3,9 @@ package com.example.aiprojectfinal.client;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 public class OpenAiClient {
@@ -12,5 +15,14 @@ public class OpenAiClient {
         this.openAiWebClient = openAiWebClient;
     }
 
+    public record SimpleResponse(String id, List<Output> output) {}
+    public record Output(String id, List<Content> content) {}
+    public record Content(String text) {}
+
+    public record SimpleRequest(String model, String input) {}
+
+    public Mono<SimpleResponse> getResponses(String prompt) {
+
+    }
 
 }
---
+++ src/main/java/com/example/aiprojectfinal/dto/TmdbResponse.java
@@ -0,0 +1,5 @@
+package com.example.aiprojectfinal.dto;
+
+import java.util.List;
+
+public record TmdbResponse(List<TmdbMovie> results) {}
